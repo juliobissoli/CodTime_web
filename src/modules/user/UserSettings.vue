5833f0d9d68d917d266b7e645f7d8604e52f4fc2
Julio Bissoli <juliobissoli33@gmail.com>
2022-02-17 10:02:22 -0300

    Add nas confirguraçẽos switch p escoler mode de acesso a helper

--- src/modules/user/UserSettings.vue
+++ src/modules/user/UserSettings.vue
@@ -1,50 +1,52 @@
 <template>
-  <section class="row bg-white ">
-    <div class="col-12  page-wrapper ">
+  <section class="row bg-white">
+    <div class="col-12 page-wrapper">
       <div class="row">
         <aside class="col-4 mt-5 p-5">
           <Avatar :item="userInfo" />
-          <footer class="d-flex justify-content-center">
+          <!-- <footer class="d-flex justify-content-center">
             <button class="mt-1 btn btn-outline-dark">
               Alterar avatar
             </button>
-          </footer>
+          </footer> -->
         </aside>
         <aside class="col-8 mt-5 pr-5">
-          <div class="row box-input divider_bottom" style="  margin-top: 6em;">
+          <div class="row box-input divider_bottom" style="margin-top: 6em">
             <label class="col-12" for="">
               Username
-              <button class="btn p-0">
+              <!-- <button class="btn p-0">
                 <i class="icon icon-edite"></i>
-              </button>
+              </button> -->
             </label>
-            <h1 class="col-12">{{userInfo.username}}</h1>
+            <h1 class="col-12">{{ userInfo.username }}</h1>
           </div>
-          <div class="row box-input divider_bottom" style="  margin-top: 6em;">
+          <div class="row box-input divider_bottom" style="margin-top: 6em">
             <label class="col-12" for="">
               Email de acesso
-              <button class="btn p-0">
+              <!-- <button class="btn p-0">
                 <i class="icon icon-edite"></i>
-              </button>
+              </button> -->
             </label>
-            <h1 class="col-12">{{userInfo.email}}</h1>
+            <h1 class="col-12">{{ userInfo.email }}</h1>
           </div>
 
-          <div class="row box-input divider_bottom" style="  margin-top: 6em;">
-            <label class="col-12 mb-4">
-              Redefinir senha
-            </label>
-            <div class="col-12 col-md-4 d-flex flex-column  mb-3">
-              <small class="text-muted">Data de entrada</small>
-              <input type="password" placeholder="Senha" class="form-control" />
+          <div class="row box-input divider_bottom" style="margin-top: 6em">
+            <label class="col-12 mb-4"> Ajudas </label>
+            <div class="col-12 pb-4">
+              <span>Url</span>
+         
             </div>
-            <div class="col-12 col-md-4 d-flex flex-column  mb-3">
-              <small class="text-muted">Data de entrada</small>
-              <input type="password" placeholder="Senha" class="form-control" />
+          </div>
+
+          <div class="row box-input divider_bottom" style="margin-top: 6em">
+            <label class="col-12 mb-4"> URL de acesso </label>
+            <div class="col-12 pb-4">
+              <span></span>
+              <BtnSwitch
+                @change-select="setFieldsHelper"
+                :labels="fields_helper || []"
+              />
             </div>
-            <div class="col-4 d-flex flex-column-reverse mb-3">
-              <button class="btn btn-outline-dark align-content-end" disabled>Salvar</button>
-              </div>
           </div>
         </aside>
       </div>
@@ -52,23 +54,34 @@
   </section>
 </template>
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 import Avatar from "../../components/utils/Avatar";
+import BtnSwitch from "../../components/utils/BtnSwitch.vue";
 export default {
   name: "UserSettings",
-  created(){
-    if(this.userInfo.id === -1){
-      this.setUser()
+  data() {
+    return {
+      fields_helper: [
+        { value: "modal", text: "Mostrar popup de ajuda" },
+        { value: "redirect", text: "Redirecionamento diretamente" },
+      ],
+    };
+  },
+  created() {
+    if (this.userInfo.id === -1) {
+      this.setUser();
     }
   },
-  components: { Avatar },
+  components: { Avatar, BtnSwitch },
   computed: {
-    ...mapGetters('user_info', ['userInfo'])
+    ...mapGetters("user_info", ["userInfo"]),
   },
   methods: {
     ...mapActions("user_info", ["setUser"]),
-
-  }
+    setFieldsHelper(event) {
+      console.log("mudo ", event);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
